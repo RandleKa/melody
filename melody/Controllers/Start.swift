2125e7946fb9e0e906cf5e0f039644c866351b05
Kammeron randle <koldkam@gmail.com>
2018-05-13 17:29:16 -0400

    AssembledViewController setup
    Basic Setup of "AssembledViewController".

---
+++ melody/Controllers/Start.swift
@@ -0,0 +1,15 @@
+//
+//  Start.swift
+//  melody
+//
+//  Created by Kammeron Randle on 5/13/18.
+//  Copyright © 2018 Kammeron Randle. All rights reserved.
+//
+
+import UIKit
+
+class melodyViewController_Start_Assembled : UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent;
+    }
+}
